647a9d28ae5b6a94e82d7b55500c12ab219536cf
Kate Hooker <katehooker88@gmail.com>
2024-06-19 19:30:23 +1200

    organising arrays

--- another.js
+++ another.js
@@ -25,27 +25,28 @@ function hourglassSum(arr) {
 
   //add first three numbers, compare to biggestHourglass,return i++
 // break when last number in the equation is 6
-  for (let i=0; i>array[0].length ; i++){
+  for (let i=0; i>array[i].length ; i++){
    
-    let firstHourglass = array[0][0] + array[0][1] + array[0][2]
-    let secondHourglass = array[0][3] + array[0][1] + array[0][2]
-    let thirdHourglass = array[0][4] + array[0][3] + array[0][2]
-    let fourthHourglass = array[0][3] + array[0][4] + array[0][5]
+    let firstHourglass = array[i][0] + array[i][1] + array[i][2] + array[i+1][1] + array[i+2][0] + array[i+2][1] + array[i+2][2] 
+    let secondHourglass = array[i][3] + array[i][1] + array[i][2]
+    let thirdHourglass = array[i][4] + array[i][3] + array[i][2]
+    let fourthHourglass = array[i][3] + array[i][4] + array[i][5]
 
- /* let arrayOfTotals = [firstHourglass, secondHourglass, thirdHourglass, fourthHourglass]
-  console.log(arrayOfTotals)
-  console.log(firstHourglass)
- let largestElement = 0 */
+  let arrayOfTotals = [firstHourglass, secondHourglass, thirdHourglass, fourthHourglass]
+  //console.log(arrayOfTotals)
+  //console.log(firstHourglass)
+  let largestElement = 0 
 
- /*for (let i = 0; i < arrayOfTotals.length; i++) {
+ for (let i = 0; i < arrayOfTotals.length; i++) {
   if (arrayOfTotals[i] > largestElement) {
       largestElement = arrayOfTotals[i];
   }
-} */
+} 
 
-return arrayOfTotals;
+}
+return largestElement;
 
-  }
+  
 }
 
 const result = hourglassSum(arr);
